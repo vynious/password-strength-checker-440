5d4cc4c3f1ca05fb2b3224344b68a6796584ab7d
vynious <shawnthiah@gmail.com>
2023-10-18 18:07:09 +0800

    exploding corgi

---
+++ nsbj-pt/src/app/components/ExplodingIcon.tsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
+
+interface Position {
+  x: number;
+  y: number;
+}
+
+const ExplodingIcon: React.FC = () => {
+  const [positions, setPositions] = useState<Position[]>([]);
+  const totalIcons = 500; // Total number of icons you want to display
+  const duration = 2; // Duration in seconds
+  const interval = duration * 1000 / totalIcons; // Calculating the interval time based on total icons and duration
+
+  useEffect(() => {
+    const generatePosition = (): Position => {
+      const x = Math.random() * (window.innerWidth - 100); // considering image width
+      const y = Math.random() * (window.innerHeight - 100); // considering image height
+      return { x, y };
+    };
+
+    const spawnInterval = setInterval(() => {
+      setPositions((prev) => [...prev, generatePosition()]);
+    }, interval);
+
+    const durationTimeout = setTimeout(() => {
+      clearInterval(spawnInterval); // Clear interval after the specified duration
+    }, duration * 1000);
+
+    return () => {
+      clearInterval(spawnInterval); // Clear interval if component unmounts early
+      clearTimeout(durationTimeout); // Clear the timeout if the component unmounts early
+    };
+  }, []);
+
+  return (
+    <div className="fixed top-0 left-0 w-screen h-screen overflow-hidden">
+      {positions.map((pos, index) => (
+        <div
+          key={index}
+          className="absolute animate-explode" // Tailwind class for animation
+          style={{ top: `${pos.y}px`, left: `${pos.x}px` }}
+        >
+          <Image src="/cool.png" width={100} height={100} alt="corgi" />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ExplodingIcon;
